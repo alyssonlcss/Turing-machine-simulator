272687cc758e08c3e02ab4cdb94c6e35e4024bbd
Lila M. <lila.msfrazao@gmail.com>
2025-01-21 23:09:57 -0300

    Alteração Main Inicial

--- main.py
+++ main.py
@@ -5,24 +5,25 @@ from typing import List, Tuple
 def turing_machine(script: str) -> Tuple[str, bool]:
     lines = [line.strip() for line in script.split("\n") if line.strip() and not line.startswith("#")]
 
-    
-    tape: List[str] = []
+    fita: List[str] = []
     initial_state: str = ""
     accept_state: str = ""
     transitions: List[Tuple[str, str, str, str, str]] = []
 
     for line in lines:
-        if line.startswith("tape"):
-            tape = list(line.split()[1])
+        if line.startswith("fita"):
+            fita = list(line.split()[1])
         elif line.startswith("init"):
             initial_state = line.split()[1]
         elif line.startswith("accept"):
             accept_state = line.split()[1]
+        elif line.startswith("#"): # Ignora comentários
+            continue
         elif "," in line:
             transitions.append(tuple(line.split(",")))
 
-    tape.insert(0, "_")
-    tape.append("_")
+    fita.insert(0, "_")
+    fita.append("_")
 
     current_state = initial_state
     head_position = 1
@@ -30,21 +31,21 @@ def turing_machine(script: str) -> Tuple[str, bool]:
     def show_tape():
         os.system('cls' if os.name == 'nt' else 'clear')
         print("Estado atual:", current_state)
-        print("tape:", "".join(tape))
+        print("tape:", "".join(fita))
         print(" " * (head_position * 2 + 6) + "^ (Cabeça)")
         time.sleep(0.3)
 
     show_tape()
 
     while True:
-        current_symbol = tape[head_position]
+        current_symbol = fita[head_position]
         transition_found = False
 
         for transition in transitions:
             state, simbolo, next_state, new_symbol, direction = transition
             if current_state == state and current_symbol == simbolo:
                 if new_symbol != '':
-                    tape[head_position] = new_symbol
+                    fita[head_position] = new_symbol
                 current_state = next_state
                 head_position += 1 if direction == ">" else -1
                 transition_found = True
@@ -55,37 +56,15 @@ def turing_machine(script: str) -> Tuple[str, bool]:
         if not transition_found:
             break
 
-    tape_final = "".join(tape).strip("_")
+    fita_final = "".join(fita).strip("_")
     input_accept = current_state == accept_state
     show_tape()
     print("\nExecução finalizada.")
-    return tape_final, input_accept
+    return fita_final, input_accept
 
+if __name__ == "__main__":
+    arquivo = open("palindromo.txt", "r")
 
-script = """
-@Programa Fonte UFC
-tape 101
-init q0
-accept qf
-reject qr
-
-q0,1,q1,_,>
-q0,0,q2,_,>
-q0,_,qf,_,>
-q1,1,q1,1,>
-q1,0,q1,0,>
-q1,_,q3,_,<
-q2,1,q2,1,>
-q2,0,q2,0,>
-q2,_,q4,_,<
-q3,1,q0,_,<
-q3,_,qf,_,>
-q3,0,qr,_,>
-q4,0,q0,_,<
-q4,_,qf,_,>
-q4,1,qr,_,>
-"""
-
-tape_final, input_accept = turing_machine(script)
-print("Fita final:", tape_final)
-print("Cadeia aceita?", input_accept)
+    fita_final, input_accept = turing_machine(arquivo)
+    print("Fita final:", fita_final)
+    print("Cadeia aceita?", input_accept)
